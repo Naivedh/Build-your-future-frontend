3247cf5c89f5286142b969f9b4ae819ad201ceed
yash shah <yashnshah97@gmail.com>
2022-04-21 16:13:44 -0500

    adding course button

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useTutor } from "../TutorContextProvider";
 
@@ -6,6 +6,7 @@ const Course = (props) => {
   const data = useTutor();
   const params = useParams();
   const navigate = useNavigate();
+  const [isEnroll, setIsEnroll] = useState(false);
 
   let course;
   for (let i = 0; i < data.instructors.length; i++) {
@@ -54,7 +55,9 @@ const Course = (props) => {
       </div>
     );
   };
-
+function changeEnroll(){
+  setIsEnroll(true);
+}
   return (
     <div className="container">
       <div className="row">
@@ -76,29 +79,33 @@ const Course = (props) => {
           <p className="tutor__tutor__name">{course.name}</p>
 
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing bibendum est ultricies integer quis auctor elit sed vulputate. Arcu risus quis varius quam. Sem et tortor consequat id porta nibh. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Senectus et netus et malesuada fames ac turpis egestas maecenas. Egestas dui id ornare arcu odio. In hac habitasse platea dictumst quisque sagittis. Dolor sit amet consectetur adipiscing elit ut aliquam. Adipiscing diam donec adipiscing tristique risus nec feugiat. Orci nulla pellentesque dignissim enim sit amet venenatis urna. Ornare lectus sit amet est placerat in egestas erat imperdiet. Convallis aenean et tortor at risus viverra adipiscing at in. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Ac tortor vitae purus faucibus ornare suspendisse sed.</p>
-<div class="text-center">
+<div class="text-center course__button">
+         {!isEnroll?(<div>
           <button
                 type="button"
                 class="btn btn-success m-4"
                 data-dismiss="modal"
+                onClick={changeEnroll}
               >
                 Enroll
               </button>
-
-              <br/> <button
+</div>):null}
+              <br/> 
+              {isEnroll ? (<div><button
                 type="button"
                 class="btn btn-warning m-4"
                 data-dismiss="modal"
               >
                 Favorite
               </button>
+
               <button
                 type="button"
                 class="btn btn-secondary m-4"
                 data-dismiss="modal"
               >
                 appointment
-              </button>
+              </button></div> ) : null}
               </div>
 
           {/* <p className="tutor__tutor__about">{tutor.about}</p>
