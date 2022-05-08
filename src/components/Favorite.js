41ed48f06e7186990f386e8d8bb1ed6a4505705a
Naivedh <naivedhshah1999@gmail.com>
2022-05-08 17:12:46 -0500

    isFavourite Done

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -4,61 +4,67 @@ import "../css/Tutor_Course.css";
 import "../css/Home.css";
 import "../css/Card.css";
 import Loader from "./Loader";
-import { httpGet } from "../utils/api";
+import { httpGet, httpPost } from "../utils/api";
+import { Link } from "react-router-dom";
 
 const Favourite = () => {
+  const [loading, setLoading] = useState(true);
   const [favourite, setFavourite] = useState();
-  const[loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       try {
         const data = await httpGet(`/studentapi/studentFavourite`);
-        console.log(data);
-        setFavourite(data[0])
+        // console.log(data);
+        setFavourite(data);
         setLoading(false);
       } catch (err) {
         console.log(err);
-        // navigate("/error");
       }
     })();
   }, []);
 
-  if(loading){
-    return<Loader/>;
+  const handleFavourite = async(courseId) => {
+    setLoading(true)
+    try {
+      const data = await httpPost(`/studentapi/studentFavourite`,{courseId});
+      setFavourite(data);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+ 
+  if (loading) {
+    return <Loader />;
   }
 
   return (
     <div className="container">
       <div className="row home__row">
-        <div className="col-lg-4">
-          <div className="card__content card__content__nohover">
-            <div className="row justify-content-end"></div>
-            <div className="row justify-content-center">
-              <img
-                src="../../static/_1.webp"
-                alt="Web Programming"
-                className="rounded-circle home__portrait"
-              />
-              <h5>Web Programming</h5>
-              <p className="card-text home__tutor__stars mb-0">
-                <small className="text-muted">
-                  <i className="bi bi-star-fill"></i>
-                  <i className="bi bi-star-fill"></i>
-                  <i className="bi bi-star-fill"></i>
-                  <i className="bi bi-star-fill"></i>
-                </small>
-              </p>
-              <p></p>
-              <a href="/course/asdklaskdlaksd">
-                <p className="btn btn-secondary card__button">View details »</p>
-              </a>
-              <a href="/course/asdklaskdlaksd">
-                <p className="btn btn-danger">Remove Course</p>
-              </a>
+       {
+          favourite.map((course)=>
+          <div className="col-lg-4" key={course._id}>
+            <div className="card__content card__content__nohover">
+              <div className="row justify-content-end"></div>
+              <div className="row justify-content-center">
+                <img
+                  src={course.courseImageUrl}
+                  alt={course.courseName}
+                  className="rounded-circle home__portrait"
+                />
+                <h5>{course.courseName}</h5>
+                <p></p>
+                <Link to={`/course/${course.courseId}`}>
+                  <p className="btn btn-secondary card__button">View details »</p>
+                </Link>
+                
+                <p className="btn btn-danger" onClick={()=>handleFavourite(course.courseId)}>Remove Course</p>
+              </div>
             </div>
           </div>
-        </div>
+          )
+       }
       </div>
     </div>
   );
